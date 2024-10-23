da5b9849f272330f8a9805b52f83773b5ab4e858
Lak5hay <tlakshay98@gmail.com>
2024-10-24 03:06:29 +0530

    Create Single_inheritance.cpp

---
+++ Single_inheritance.cpp
@@ -0,0 +1,31 @@
+
+//PRN: 23070123079
+// single inheritance
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Uni {
+    public:
+    string university="Symbiosis International University: ";
+    void discipline()
+    {
+        cout<<"Engineering"<<endl;
+    }
+};
+class Department: public 
+    Uni 
+    {
+    public:
+    string dept="Electronics & Communication";
+    };
+
+int main(){
+    Department a1;
+    a1.discipline();
+    cout<<a1.university+" "+a1.dept;
+}
+/*
+Engineering
+Symbiosis International University:  Electronics & Communication
+*/
